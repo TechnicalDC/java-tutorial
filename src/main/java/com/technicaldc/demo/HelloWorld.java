472218626bc749c09e727a653f2c2b8d26cbe4aa
Dilip Chauhan <chauhandilip305@gmail.com>
2025-06-18 19:56:22 +0530

    updated

--- src/main/java/com/technicaldc/demo/HelloWorld.java
+++ src/main/java/com/technicaldc/demo/HelloWorld.java
@@ -1,7 +1,20 @@
 package com.technicaldc.demo;
 
 public class HelloWorld {
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		// Primitive: int, long, short, byte, char, float, double, boolean
+		int variable = 28;
+		byte age = 28;
+		// Can use _ instead of comma in the number
+		// adding L as suffix so that java dont consider it as int by default
+		long viewCount = 3_123_456_789L;
+		// adding F as suffix so that java dont consider it as double by default
+		float price = 10.99F;
+		char lang = 'E';
+		boolean isActive = true;
+		String hello = "Hello World";
+
+		System.out.println(hello);
 	}
 }
