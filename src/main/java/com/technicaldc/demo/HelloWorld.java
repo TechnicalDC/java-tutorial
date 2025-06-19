e6a24caf382c99f7ce04915bb356ba73eb2778e4
Dilip Chauhan <chauhandilip305@gmail.com>
2025-06-19 13:33:32 +0530

    updated

--- src/main/java/com/technicaldc/demo/HelloWorld.java
+++ src/main/java/com/technicaldc/demo/HelloWorld.java
@@ -1,20 +1,31 @@
 package com.technicaldc.demo;
 
 public class HelloWorld {
-	@SuppressWarnings("unused")
+	/* @SuppressWarnings("unused") */
 	public static void main(String[] args) {
 		// Primitive: int, long, short, byte, char, float, double, boolean
-		int variable = 28;
-		byte age = 28;
+		// int variable = 28;
+		// byte age = 28;
 		// Can use _ instead of comma in the number
 		// adding L as suffix so that java dont consider it as int by default
-		long viewCount = 3_123_456_789L;
+		// long viewCount = 3_123_456_789L;
 		// adding F as suffix so that java dont consider it as double by default
-		float price = 10.99F;
-		char lang = 'E';
-		boolean isActive = true;
-		String hello = "Hello World";
+		// float price = 10.99F;
+		// char lang = 'E';
+		// boolean isActive = true;
+		// String hello = "Hello World";
+		final float PI = 3.14F;
+		String[] code = { "a", "b", "c", "d" };
+		int[] numbers = new int[5];
+		numbers[0] = 1;
+		numbers[1] = 2;
+		numbers[2] = 3;
+		numbers[3] = 4;
+		numbers[4] = 5;
 
-		System.out.println(hello);
+		// Casting:
+		double result = (double) 10 / (double) 3;
+
+		// System.out.println(hello);
 	}
 }
